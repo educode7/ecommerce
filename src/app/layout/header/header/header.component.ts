7998d351cebcc81e6fad1c894d068cdee66c65e0
apoyo06332024 <apoyo06332024@DRET1305>
2024-05-27 15:44:20 -0500

    fix agregar directivas al componente header

--- src/app/layout/header/header/header.component.ts
+++ src/app/layout/header/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { CartStore } from '../../../shared/store/shopping-cart.store';
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe, NgClass, SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgClass, CurrencyPipe, SlicePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
