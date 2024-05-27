ab104836d8129c8b0050061232ed6162a2eca324
apoyo06332024 <apoyo06332024@DRET1305>
2024-05-27 15:38:59 -0500

    Agregar componenete header y product store

--- src/app/layout/header/header/header.component.ts
+++ src/app/layout/header/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CartStore } from '../../../shared/store/shopping-cart.store';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  showCart = signal<boolean>(false);
+  cartStore = inject(CartStore);
 }
--- src/app/shared/store/shopping-cart.store.ts
+++ src/app/shared/store/shopping-cart.store.ts
@@ -1,2 +1,68 @@
-export interface ShoppingCartStore {
+import { computed, inject } from '@angular/core';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+
+import { ToastrService } from 'ngx-toastr';
+import { ProductDto } from '../../infraestructure/dtos/product.dto';
+
+export interface CartStore {
+  products: ProductDto[];
+  totalAmount: number;
+  productsCount: number;
+}
+
+const initialState: CartStore = {
+  products: [],
+  totalAmount: 0,
+  productsCount: 0,
+};
+
+export const CartStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withComputed(({ products }) => ({
+    productsCount: computed(() => calculateProductCount(products())),
+    totalAmount: computed(() => calculateTotalAmount(products())),
+  })),
+  withMethods(({ products, ...store }, toastSvc = inject(ToastrService)) => ({
+    addToCart(product: ProductDto) {
+      const isProductInCart = products().find(
+        (item: ProductDto) => item.id === product.id
+      );
+
+      if (isProductInCart) {
+        isProductInCart.qty++;
+        isProductInCart.subTotal = isProductInCart.qty * isProductInCart.price;
+        patchState(store, { products: [...products()] });
+      } else {
+        patchState(store, { products: [...products(), product] });
+      }
+      toastSvc.success('Product added', 'DOMINI STORE');
+    },
+    removeFromCart(id: number) {
+      const updatedProducts = products().filter((product) => product.id !== id);
+      patchState(store, { products: updatedProducts });
+      toastSvc.info('Product removed', 'DOMINI STORE');
+    },
+    clearCart() {
+      patchState(store, initialState);
+      toastSvc.info('Cart cleared', 'DOMINI STORE');
+    },
+  }))
+);
+
+function calculateTotalAmount(products: ProductDto[]): number {
+  return products.reduce(
+    (acc, product) => acc + product.price * product.qty,
+    0
+  );
+}
+
+function calculateProductCount(products: ProductDto[]): number {
+  return products.reduce((acc, product) => acc + product.qty, 0);
 }
